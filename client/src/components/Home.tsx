693cce8c6a25a5d099b65582240547d4c201b7da
snavemail <98936310+snavemail@users.noreply.github.com>
2023-12-25 16:54:13 -0500

    Create a home dashboard page

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,15 +1,84 @@
-import React from 'react'
-import { logout, getUserPlayingTrack } from '../spotify'
+import React, { useEffect, useState } from 'react'
+import {
+  getProfile,
+  logout,
+  getTopItems,
+  getSavedTracks,
+  getSavedTracksByDate,
+} from '../spotify'
+import Loader from './Loader'
+import { AxiosResponse } from 'axios'
+import { styled } from 'styled-components'
+import { Main, mixins, theme } from '../styles'
+
+const HomeWrapper = styled(Main)`
+  ${mixins.flexCenter}
+  flex-direction: column
+`
+
+const DisplayName = styled.h1`
+  margin-top: 30px;
+  font-size: ${theme.fontSizes.xxl};
+`
+
+const LogoutButton = styled.button`
+  background-color: ${theme.colors.spotifyGreen};
+  color: ${theme.colors.white};
+  padding-left: ${theme.spacing.base};
+  padding-right: ${theme.spacing.base};
+  margin-top: 20px;
+`
+
+export const AvatarWrapper = styled.div`
+  width: 150px;
+  height: 150px;
+  img {
+    border-radius: 100%;
+  }
+`
 
 export default function Home({ token }: { token: string }) {
+  const [user, setUser] = useState<AxiosResponse<any, any> | null>(null)
+  const [savedTracks, setSavedTracks] = useState<
+    { added_at: string; track: any }[] | null
+  >(null)
+  const [month, setMonth] = useState<number>(9)
+  const [year, setYear] = useState<number>(2023)
+
+  useEffect(() => {
+    const getMyProfile = async () => {
+      const myUser = await getProfile()
+      setUser(myUser)
+    }
+
+    const getMySavedTracks = async () => {
+      const tracks = await getSavedTracksByDate(month, year)
+      setSavedTracks(tracks)
+    }
+
+    getMySavedTracks()
+    getMyProfile()
+  }, [month, year])
+
   return (
-    <div style={{ width: '100%', alignItems: 'center' }}>
-      <h1>Home Page</h1>
-      <p>{token}</p>
-      <button onClick={logout}>Logout</button>
-      <button onClick={() => getUserPlayingTrack({ token })}>
-        Get user Playing track
-      </button>
-    </div>
+    <HomeWrapper>
+      {user ? (
+        <>
+          <AvatarWrapper>
+            <img
+              src={
+                user.data.images[1] ? user.data.images[1].url : user.data.images[0].url
+              }
+              alt={`profile pic for ${user.data.display_name}`}
+            />
+          </AvatarWrapper>
+          <DisplayName>{user.data.display_name}</DisplayName>
+          <LogoutButton onClick={logout}>Logout</LogoutButton>
+          <p>{savedTracks ? savedTracks.length : 'loading will be here'}</p>
+        </>
+      ) : (
+        <Loader />
+      )}
+    </HomeWrapper>
   )
 }
