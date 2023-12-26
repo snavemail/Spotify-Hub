7cf2039447312802e4260733c866c3a985871a9b
snavemail <98936310+snavemail@users.noreply.github.com>
2023-12-26 00:19:55 -0500

    Create pause and play icons for liked song card

---
+++ client/src/icons/Pause.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const IconPause = () => (
+  <svg
+    version="1.1"
+    id="_x32_"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    xmlSpace="preserve"
+    width="100%"
+    height="100%"
+    fill="#ffffff"
+  >
+    <style type="text/css">{`.st0{fill:#ffffff;}`}</style>
+    <g>
+      <rect y="20.885" className="st0" width="188.088" height="470.23" />
+      <rect x="323.912" y="20.885" className="st0" width="188.088" height="470.23" />
+    </g>
+  </svg>
+)
+
+export default IconPause
