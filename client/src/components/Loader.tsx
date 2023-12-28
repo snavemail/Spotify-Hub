6de138e8d51367c7925155d25c3bfa924a1cab2b
snavemail <98936310+snavemail@users.noreply.github.com>
2023-12-28 05:07:04 -0500

    Made mini loader in case

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -10,7 +10,7 @@ const Container = styled.div`
   flex-direction: column;
 `
 
-const dance = keyframes`
+const Dance = keyframes`
   from {
     height: 11px;
   }
@@ -27,17 +27,18 @@ const Bars = styled.div`
   min-width: 100px;
   height: 52px;
   margin: 0 auto;
-  z-index: 2;
+  z-index: 1;
   position: relative;
   left: 0;
   right: 0;
 `
+
 const Bar = styled.div<{ delay: string }>`
   width: 10px;
   height: 5px;
   margin: 0 2px;
   background-color: ${colors.offGreen};
-  animation-name: ${dance};
+  animation-name: ${Dance};
   animation-duration: 400ms;
   animation-play-state: running;
   animation-direction: alternate;
@@ -45,6 +46,55 @@ const Bar = styled.div<{ delay: string }>`
   animation-iteration-count: infinite;
   animation-delay: ${(props) => props.delay || '0ms'};
 `
+
+const MiniDance = keyframes`
+  from {
+    height: 8px;
+  }
+  to {
+    height: 100%;
+  }
+`
+
+const MiniBars = styled.div`
+  display: flex;
+  align-items: flex-end;
+  overflow: hidden;
+  width: 100px;
+  min-width: 100px;
+  height: 25px;
+  margin: 0 auto;
+  z-index: 1;
+  position: relative;
+  left: 0;
+  right: 0;
+`
+
+const MiniBar = styled.div<{ delay: string }>`
+  width: 8px;
+  height: 5px;
+  margin: 0 2px;
+  background-color: ${colors.offGreen};
+  animation-name: ${MiniDance};
+  animation-duration: 400ms;
+  animation-play-state: running;
+  animation-direction: alternate;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  animation-delay: ${(props) => props.delay || '0ms'};
+`
+
+export function MiniLoader() {
+  return (
+    <MiniBars>
+      <MiniBar delay="250ms" />
+      <MiniBar delay="715ms" />
+      <MiniBar delay="475ms" />
+      <MiniBar delay="25ms" />
+      <MiniBar delay="190ms" />
+    </MiniBars>
+  )
+}
 
 export default function Loader() {
   return (
