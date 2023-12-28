f58096cc676c7cb1c54b03cc083487bcd24be656
snavemail <98936310+snavemail@users.noreply.github.com>
2023-12-28 05:01:50 -0500

    Move main content and navbar to separate files

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react'
-import { getProfile, getSavedTracksByDate } from '../spotify'
+import { getProfile } from '../spotify'
 import { styled } from 'styled-components'
-import { media, theme } from '../styles'
-import Loader from './Loader'
-import { LikedTrackInterface, UserInterface } from '../types'
-import User from './User'
-import LikedSong from './LikedSong'
+import { media } from '../styles'
+import { UserInterface } from '../types'
+import { MainContent } from './MainContent'
+import { LeftNavbar } from './LeftNavbar'
 
 const Container = styled.div`
   display: flex;
@@ -15,100 +14,6 @@ const Container = styled.div`
   flex-direction: column;
 `};
 `
-
-const Navbar = styled.div`
-  display: flex;
-  width: 400px;
-  flex-direction: column;
-  background-color: ${theme.colors.black};
-  color: ${theme.colors.white};
-  margin: 8px 0px 8px 8px;
-  border-radius: 19px;
-  gap: 8px;
-
-  .playlist {
-    height: 500px;
-  }
-
-  ${media.lgtablet`
-  margin: 8px 8px 0px 8px;
-  width: calc(100vw - 16px);
-`};
-`
-
-const NavbarContentTop = styled.div`
-  overflow-y: auto;
-  background-color: ${theme.colors.darkGrey};
-  border-radius: 19px;
-  ${media.lgtablet`
-  overflow-y: visible;
-`};
-`
-
-const NavbarContentBottom = styled.div`
-  flex: 1;
-  overflow-y: auto;
-  background-color: ${theme.colors.darkGrey};
-  border-radius: 19px;
-  ${media.lgtablet`
-  flex: unset;
-  minHeight: 500px;
-`};
-`
-
-const LeftNavbar = ({ user }: { user: UserInterface | null }) => {
-  return (
-    <Navbar>
-      <NavbarContentTop>{user ? <User user={user} /> : <Loader />}</NavbarContentTop>
-      <NavbarContentBottom className="playlist">{<Loader />}</NavbarContentBottom>
-    </Navbar>
-  )
-}
-
-const Content = styled.div`
-  flex: 1;
-  padding: 8px;
-  overflow-y: auto;
-  margin: 8px;
-  background-color: ${theme.colors.darkGrey};
-  border-radius: 19px;
-
-  ${media.lgtablet`
-  overflow-y: visible;
-`};
-`
-
-const MainContent = () => {
-  const [savedTracks, setSavedTracks] = useState<LikedTrackInterface[] | null>(null)
-  const [month, setMonth] = useState<number>(12)
-  const [year, setYear] = useState<number>(2023)
-
-  useEffect(() => {
-    const getMySavedTracks = async () => {
-      const tracks = await getSavedTracksByDate(month, year)
-      setSavedTracks(tracks)
-    }
-
-    getMySavedTracks()
-  }, [month, year])
-
-  return (
-    <Content>
-      <h1>Liked Songs</h1>
-      {savedTracks ? (
-        <div>
-          <ol>
-            {savedTracks.map((savedTrack, index) => (
-              <LikedSong key={index} track={savedTrack.track} />
-            ))}
-          </ol>
-        </div>
-      ) : (
-        <Loader />
-      )}
-    </Content>
-  )
-}
 
 export default function Home() {
   const [user, setUser] = useState<UserInterface | null>(null)
