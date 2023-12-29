17136f1c69715024a24a488aafcfd025c1f7917a
snavemail <98936310+snavemail@users.noreply.github.com>
2023-12-29 02:47:03 -0500

    Create Playlist component

--- client/src/components/Playlist.tsx
+++ client/src/components/Playlist.tsx
@@ -1,5 +1,36 @@
 import React from 'react'
+import { PlaylistInterface } from '../types'
+import { styled } from 'styled-components'
 
-export default function Playlist() {
-  return <div>Playlist</div>
+const PlaylistContainer = styled.div`
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  padding: 8px;
+`
+
+const ImageContainer = styled.img`
+  width: 69px;
+  height: 69px;
+`
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const NameContainer = styled.div``
+
+const InfoContainer = styled.div``
+
+export default function Playlist({ playlist }: { playlist: PlaylistInterface }) {
+  return (
+    <PlaylistContainer>
+      {playlist.images.length && <ImageContainer src={playlist.images[0].url} />}
+      <TextContainer>
+        <NameContainer>{playlist.name}</NameContainer>
+        <InfoContainer>{`${playlist.owner.display_name} -> ${playlist.tracks.total}`}</InfoContainer>
+      </TextContainer>
+    </PlaylistContainer>
+  )
 }
--- client/src/components/PlaylistList.tsx
+++ client/src/components/PlaylistList.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function PlaylistList() {
-  return <div>PlaylistList</div>
+  return
 }
