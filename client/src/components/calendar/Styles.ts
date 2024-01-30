94fc2f7138fb68dde3ff2ea2d9138b5a47f552cc
snavemail <98936310+snavemail@users.noreply.github.com>
2024-01-30 12:59:56 -0500

    Make general styles for calendar

---
+++ client/src/components/calendar/Styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+  letter-spacing: -0.025em;
+  margin: 0 0 10px;
+  font-weight: 700;
+`
